17f9148b2747523d01cdc001e2b5050d49d6273e
mainul hasan <mainulhasan@iut-dhaka.edu>
2023-05-21 07:57:10 +0600

    Implemented combat matach scene

--- Assets/Alif_Joystick.cs
+++ Assets/Alif_Joystick.cs
@@ -15,7 +15,10 @@ public class Alif_Joystick : MonoBehaviour
     float jumpSpeed = 5f;
     [SerializeField]
     float dirx;
-
+    public GameObject attackpoint1, attackpoint2, attackpoint3, attackpoint4, attackpoint5;
+    public float attacRadius1;
+    public float attacRadius2, attacRadius3, attacRadius4, attacRadius5;
+    public LayerMask opponents;
     private bool isRight;
 
     // Start is called before the first frame update
@@ -33,7 +36,16 @@ public class Alif_Joystick : MonoBehaviour
         //transform.Translate(Input.GetAxis("Horizontal1") * runSpeed,0, rb2d.velocity.y);
         float dirx = Input.GetAxisRaw("Horizontal");
         rb2d.velocity = new Vector2(dirx * runSpeed, rb2d.velocity.y);
+        if (dirx == 0f)
+        {
 
+
+        }
+        else if (dirx > 0f || dirx < 0f)
+        {
+            Debug.Log("asd");
+            animator.Play("p1_walk");
+        }
 
         if (Input.GetButtonDown("Jump1"))
         {
@@ -68,5 +80,57 @@ public class Alif_Joystick : MonoBehaviour
     private void flip()
     {
 
+    }
+    public void attack1()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint1.transform.position, attacRadius1, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(9);
+        }
+    }
+    public void attack2()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint2.transform.position, attacRadius2, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(13);
+        }
+    }
+    public void attack3()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint3.transform.position, attacRadius3, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(20);
+        }
+    }
+    public void attack4()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint4.transform.position, attacRadius4, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(4);
+        }
+    }
+    //public void attack5()
+    //{
+    //    Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint5.transform.position, attacRadius5, opponents);
+    //    foreach (Collider2D opponentObject in opponent)
+    //    {
+    //        damage(20);
+    //    }
+    //}
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackpoint1.transform.position, attacRadius1);
+        Gizmos.DrawWireSphere(attackpoint2.transform.position, attacRadius2);
+        Gizmos.DrawWireSphere(attackpoint3.transform.position, attacRadius3);
+        Gizmos.DrawWireSphere(attackpoint4.transform.position, attacRadius4);
+        Gizmos.DrawWireSphere(attackpoint5.transform.position, attacRadius5);
+    }
+    void damage(int d)
+    {
+        staticClass.player1Health -= d;
     }
 }
--- Assets/characterS/Joe Higashi/player_movement.cs
+++ Assets/characterS/Joe Higashi/player_movement.cs
@@ -16,7 +16,10 @@ public class player_movement : MonoBehaviour
     float jumpSpeed = 5f;
     [SerializeField]
     float dirx;
-   
+    public GameObject attackpoint1, attackpoint2, attackpoint3, attackpoint4, attackpoint5;
+    public float attacRadius1;
+    public float attacRadius2, attacRadius3, attacRadius4, attacRadius5;
+    public LayerMask opponents;
     private bool isRight;
 
 
@@ -32,18 +35,19 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Input.GetAxis("Horizontal1") * runSpeed,0, rb2d.velocity.y);
+        
         float dirx = Input.GetAxisRaw("Horizontal");
         rb2d.velocity= new Vector2(dirx*runSpeed, rb2d.velocity.y);
-      
+        
 
+
+        flip();
         if (Input.GetButtonDown("Jump1"))
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
             animator.Play("p1_Jump");
         }
 
-        flip();
         if (Input.GetButtonDown("jFire1"))
         {
 
@@ -69,6 +73,64 @@ public class player_movement : MonoBehaviour
     }
     private void flip()
     {
-       
+        if (!isRight && dirx<0f || isRight && dirx>0f)
+        {
+            isRight = !isRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    public void attack1()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint1.transform.position, attacRadius1, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(9);
+        }
+    }
+    public void attack2()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint2.transform.position, attacRadius2, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(13);
+        }
+    }
+    public void attack3()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint3.transform.position, attacRadius3, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(20);
+        }
+    }
+    public void attack4()
+    {
+        Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint4.transform.position, attacRadius4, opponents);
+        foreach (Collider2D opponentObject in opponent)
+        {
+            damage(4);
+        }
+    }
+    //public void attack5()
+    //{
+    //    Collider2D[] opponent = Physics2D.OverlapCircleAll(attackpoint5.transform.position, attacRadius5, opponents);
+    //    foreach (Collider2D opponentObject in opponent)
+    //    {
+    //        damage(20);
+    //    }
+    //}
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackpoint1.transform.position, attacRadius1);
+        Gizmos.DrawWireSphere(attackpoint2.transform.position, attacRadius2);
+        Gizmos.DrawWireSphere(attackpoint3.transform.position, attacRadius3);
+        Gizmos.DrawWireSphere(attackpoint4.transform.position, attacRadius4);
+        Gizmos.DrawWireSphere(attackpoint5.transform.position, attacRadius5);
+    }
+    void damage(int d)
+    {
+        staticClass.player1Health -= d;
     }
 }
